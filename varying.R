9a91243124e4f29cfea0f825b1ee225beeca52c9
Jonathan Dushoff <dushoff@mcmaster.ca>
2017-05-03 18:45:35 -0400

    Autocommit (scratch)

---
+++ varying.R
@@ -0,0 +1,22 @@
+  library(mgcv)
+
+  N<- 100
+  N.distclass<- 7
+
+  X<- matrix(runif(N * N.distclass, -2, 2), N, N.distclass)
+
+  noise<- rnorm(N)
+
+  true.coefs<- c(0.3, 0.5, 0.7, 0.8, 0.6, 0.3, 0.1)
+
+  y<- X %*% true.coefs + noise
+
+  D<- matrix(1:N.distclass, N, N.distclass, byrow= T)
+
+  b<- gam(y ~ s(D, by= X, k= 3))
+  summary(b)
+
+  par(mfrow= c(1, 1))
+  plot(b, shade= TRUE)
+  lines(1:N.distclass, true.coefs, col= 2)
+
